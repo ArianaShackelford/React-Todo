3f3adcb1f613a20f10c4b4d30ce09e41a2f378d6
Ariana Shackelford <ariana.shackelford@gmail.com>
2019-12-02 20:57:17 -0500

    set up state and add Todo to app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from 'react';
 
+const todos = [
+  {
+    name: 'Do Laundry',
+    id: 123,
+    completed: false,
+  }
+]
+
 class App extends React.Component {
   // you will need a place to store your state in this component.
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+    super();
+    this.state = {
+      todos: todos,
+      anotherTodo: '',
+    };
+  };
+addTodo = newTodoText => {
+  const newTodo = {
+    name: newTodoText,
+    id: Date.now(),
+    completed: false,
+  };
+  this.setState({
+    todo: [...this.state.todos, newTodo]
+  });
+  };
+
   render() {
+    console.log('rendering...');
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <h2>Time ToDo Me!</h2>
       </div>
     );
   }
